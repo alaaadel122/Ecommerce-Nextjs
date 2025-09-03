e2faf894b3b166b1552747b2c399ce22b1dbb1e3
alaaadel122 <alaaadel109@gmail.com>
2025-09-03 12:10:23 +0300

    cart number

--- src/app/_components/ShoppingCart.tsx
+++ src/app/_components/ShoppingCart.tsx
@@ -15,7 +15,7 @@ export default function ShoppingCart() {
           <div className='relative'>
                <i  className='relative fa-solid fa-cart-shopping fa-xl' ></i>
                <div className='absolute bottom-3 left-3 size-5 flex justify-center items-center bg-main rounded-full '>
-                    <span className='text-light cart-counter'>{data?.numOfCartItems}</span>
+                    <span className='text-light cart-counter'>{data?.numOfCartItems ? data?.numOfCartItems :0}</span>
                </div>
 
           </div>
