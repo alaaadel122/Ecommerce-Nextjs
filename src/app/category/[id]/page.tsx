0bbad0a4d94f5b1125a1a8364fd7e0ccf29e0110
alaaadel122 <alaaadel109@gmail.com>
2025-09-09 23:08:12 +0300

    category

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -7,8 +7,9 @@ import { da } from 'zod/v4/locales';
 export default async function page({params}:{params:Promise<{id:string}>}) {
   const { id } = await params // id distrct from folder [id] so i can't change the name 
   const data: Category = await grtSpecificCategory(id);
-  return (<>
+  console.log(data)
+  return (<div className='flex justify-center items-center min-h-50'>
     <h3>{data.name}</h3>
-  </>
+  </div>
   )
 }
