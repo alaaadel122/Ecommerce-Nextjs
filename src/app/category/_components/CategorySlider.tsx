4fe437d5a3b8c72953e36ef018ccd973852f45d6
alaaadel122 <alaaadel109@gmail.com>
2025-09-10 15:04:13 +0300

    categoryslider

--- src/app/category/_components/CategorySlider.tsx
+++ src/app/category/_components/CategorySlider.tsx
@@ -5,16 +5,26 @@ import { CatergoriesInterface } from '@/interfaces/categories.interface'
 
 export default async function CategorySlider() {
      const data: CatergoriesInterface[] = await grtAllCategory()
+
      return (
-          <div className='container'>
-               <h3 className="text-gray-700 text-2xl after:ml-0.5 before:text-main before:content-['|'] my-5">Category</h3>
-               <div className="overflow-x-auto ">
-                    <div className="grid grid-rows-2 grid-flow-col gap-4 overflow-hidden">
-                         {data?.map(category =>
-                              <CategorySingleItem key={category._id} category={category} catergoryId = {category._id} />
-                         )}
+          <div className="container">
+               <h3 className="text-gray-700 text-2xl after:ml-0.5 before:text-main before:content-['|'] my-5">
+                    Category
+               </h3>
+
+               <div className="overflow-x-auto overflow-y-hidden md:overflow-x-visible md:overflow-y-visible">
+                    <div className="flex flex-row gap-4 md:grid md:grid-rows-2 md:grid-flow-col">
+                         {data?.map(category => (
+                              <CategorySingleItem
+                                   key={category._id}
+                                   category={category}
+                                   catergoryId={category._id}
+                              />
+                         ))}
                     </div>
                </div>
           </div>
+
+
      )
 }
